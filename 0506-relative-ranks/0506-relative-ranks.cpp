946fe01ad0885f24233863e9711d7cd551722cc9
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-05-08 17:17:10 +0530

    Time: 4 ms (89.65%), Space: 13.7 MB (70.05%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<score.size();i++)
+        {
+            pq.push({score[i],i});
+        }
+        int i=0;
+        vector<string>ans(size(score));
+        while(!pq.empty())
+        {
+            ans[pq.top().second]=i==0 ?"Gold Medal":i==1 ? "Silver Medal":i==2 ? "Bronze Medal": to_string(i+1);
+            i++;
+            pq.pop();
+        }
+        return ans;
+    }
+};
