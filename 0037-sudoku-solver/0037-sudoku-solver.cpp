07edd5400c20051d0ab837977a4d060e389c5b4a
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-05-31 17:05:42 +0530

    Time: 17 ms (19.53%), Space: 8.7 MB (17.10%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,76 @@
+class Solution {
+    int n=9;
+public:
+    bool isEmpty(vector<vector<char>>&board,int &row,int &col)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]=='.')
+                {
+                    row=i;
+                    col=j;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    bool isSafe(int row,int col,int val,vector<vector<char>>&board)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(board[i][col]==val)
+            {
+                return false;
+            }
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(board[row][j]==val)
+            {
+                return false;
+            }
+        }
+        int rowF=row-(row%3);
+        int colF=col-(col%3);
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                if(board[i+rowF][j+colF]==val)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+        return true;
+    }
+    bool isSudoko(vector<vector<char>>&board)
+    {
+        int row=0,col=0;
+        if(!isEmpty(board,row,col))
+        {
+            return true;
+        }
+        for(char i='1';i<='9';i++)
+        {
+            if(isSafe(row,col,i,board))
+            {
+                board[row][col]=i;
+                if(isSudoko(board))
+                {
+                    return true;
+                }
+                board[row][col]='.';
+            }
+        }
+        return false;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        isSudoko(board);
+    }
+};
