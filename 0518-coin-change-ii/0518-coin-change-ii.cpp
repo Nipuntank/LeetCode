4fbf188a6ea4a326305219cdabaff960c2b1eb5e
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-06-24 22:53:46 +0530

    Time: 9 ms (59.26%), Space: 19.6 MB (67.49%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int helper(vector<int>&coins,vector<vector<int>>&dp,int i,int amt)
-    {
-        if(amt==0)
-        {
-            return 1;
-        }
-        if(i<0 || amt<0)
-        {
-            return 0;
-        }
-        if(dp[i][amt]!=-1)
-        {
-            return dp[i][amt];
-        }
-        int a=helper(coins,dp,i-1,amt);
-        int b=helper(coins,dp,i,amt-coins[i]);
-        return dp[i][amt]=(a+b);
-    }
-    int change(int amount, vector<int>& coins) {
+    
+    int change(int amt, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return helper(coins,dp,n-1,amount);
+        vector<vector<int>>dp(n,vector<int>(amt+1,0));
+        for(int i=0;i<amt+1;i++)
+        {
+            if(i%coins[0]==0)
+            {
+                dp[0][i]=1;
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<amt+1;j++)
+            {
+                int a=dp[i-1][j];
+                int b=0;
+                if(coins[i]<=j)
+                {
+                    b=dp[i][j-coins[i]];
+                }
+                dp[i][j]=a+b;
+            }
+        }
+        return dp[n-1][amt];
     }
 };
