c828a42524a74748f18125f83f42731614dd44f9
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-06-27 17:45:17 +0530

    Time: 14 ms (77.89%), Space: 19.6 MB (76.37%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    int helper(string s1,string s2,vector<vector<int>>&dp,int i,int j)
-    {
-        if(j<0)
-        {
-            return 1;
-        }
-        if(i<0)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(s1[i]==s2[j])
-        {
-            int a=helper(s1,s2,dp,i-1,j-1);
-            int b=helper(s1,s2,dp,i-1,j);
-            return dp[i][j]=(a+b)%mod;
-        }
-        else{
-            return dp[i][j]=helper(s1,s2,dp,i-1,j);
-        }
-    }
     int numDistinct(string s, string t) {
         int m=s.size();
         int n=t.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return helper(s,t,dp,m-1,n-1);
+        int mod=1e9+7;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++)
+        {
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%mod;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
