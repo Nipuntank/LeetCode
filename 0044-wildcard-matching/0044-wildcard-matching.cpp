87191601571a43fdbe8664d67f16816d12f15c56
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-06-28 16:05:35 +0530

    Time: 27 ms (78.92%), Space: 31.4 MB (26.73%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,43 +2,38 @@ class Solution {
 public:
     bool allStar(int i,string &p)
     {
-        for(int j=0;j<=i;j++)
+        for(int j=1;j<=i;j++)
         {
-            if(p[j]!='*')
+            if(p[j-1]!='*')
             {
                 return false;
             }
         }
         return true;
     }
-    bool helper(string &s,string &p,vector<vector<int>>&dp,int i,int j)
-    {
-        if(j<0)
-        {
-            return i<0;
-        }
-        if(i<0)
-        {
-            return allStar(j,p);
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(s[i]==p[j] || p[j]=='?')
-        {
-            return dp[i][j]=helper(s,p,dp,i-1,j-1);
-        }
-        if(p[j]=='*')
-        {
-            return dp[i][j]=helper(s,p,dp,i-1,j)|| helper(s,p,dp,i,j-1);    
-        }    
-        return dp[i][j]=false;
-    }
     bool isMatch(string s, string p) {
         int m=s.size();
         int n=p.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return helper(s,p,dp,m-1,n-1);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        dp[0][0]=true;
+        for(int j=1;j<n+1;j++)
+        {
+            dp[0][j]=allStar(j,p);
+        }
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==p[j-1] || p[j-1]=='?')
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                if(p[j-1]=='*')
+                {
+                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
