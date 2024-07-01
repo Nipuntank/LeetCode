6e2e68f2907de04dabfe5a91c487cd0512c73962
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-07-01 14:01:10 +0530

    Time: 3 ms (72.47%), Space: 14.1 MB (53.89%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    int helper(vector<int>&prices,vector<vector<int>>&dp,int i,int j)
-    {
-        if(i>=prices.size())
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int p;
-        if(j==0)
-        {
-            p=max(helper(prices,dp,i+1,0),helper(prices,dp,i+1,1)-prices[i]);
-        }
-        if(j==1)
-        {
-            p=max(helper(prices,dp,i+1,1),helper(prices,dp,i+2,0)+prices[i]);
-        }
-        return dp[i][j]=p;
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        return helper(prices,dp,0,0);
+        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                int p;
+                if(j==0)
+                {
+                    p=max(dp[i+1][0],dp[i+1][1]-prices[i]);
+                }
+                if(j==1)
+                {
+                    p=max(dp[i+1][1],dp[i+2][0]+prices[i]);
+                }
+                dp[i][j]=p;
+            }
+        }
+        return dp[0][0];
     }
 };
