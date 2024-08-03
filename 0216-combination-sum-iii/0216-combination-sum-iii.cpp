e301fdce4b007ef78fbb69f6c302b2fc44e8e57f
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-08-03 20:11:56 +0530

    Time: 3 ms (50.48%), Space: 9.9 MB (14.05%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void helper(int i,int sum,int k,vector<vector<int>>&ans,vector<int>temp)
+    vector<vector<int>>ans;
+    void helper(int i,int k,int sum,vector<int>temp)
     {
-        if(sum==0 && temp.size()==k)
+        if(temp.size()==k && sum==0)
         {
             ans.push_back(temp);
             return ;
         }
-        for(int j=i;j<=sum,j<=9;j++)
+        for(int j=i;j<=9;j++)
         {
             temp.push_back(j);
-            helper(j+1,sum-j,k,ans,temp);
+            helper(j+1,k,sum-j,temp);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>>ans;
         vector<int>temp;
-        helper(1,n,k,ans,temp);
+        helper(1,k,n,temp);
         return ans;
     }
 };
