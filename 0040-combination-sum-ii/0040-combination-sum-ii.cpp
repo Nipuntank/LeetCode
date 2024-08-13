83662c05c3824baec3fbc6dc5fc3e81e8096cef3
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-08-13 10:53:00 +0530

    Time: 0 ms (100.00%), Space: 14.4 MB (20.10%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void helper(vector<int>&candidates,int target,vector<int>temp,int i)
+    void helper(vector<int>&candidates,int target,int i,vector<int>temp)
     {
         if(target==0)
         {
@@ -12,21 +12,21 @@ public:
         {
             if(target<candidates[j])
             {
-                break ;
+                break;
             }
             if(j>i && candidates[j]==candidates[j-1])
             {
                 continue;
             }
             temp.push_back(candidates[j]);
-            helper(candidates,target-candidates[j],temp,j+1);
+            helper(candidates,target-candidates[j],j+1,temp);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<int>temp;
         sort(candidates.begin(),candidates.end());
-        helper(candidates,target,temp,0);
+        helper(candidates,target,0,temp);
         return ans;
     }
 };
