48ca190c3f3511916565343f22fd7f6d24812477
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-08-14 15:20:30 +0530

    Time: 4 ms (63.37%), Space: 11.3 MB (35.74%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,9 +1,10 @@
 class Solution {
+public:
     vector<vector<string>>ans;
     vector<string>temp;
-public:
-    bool isPossible(int row,int col,int n,vector<vector<int>>&board)
+    bool isPossible(int row,int col,vector<vector<int>>&board,int n)
     {
+        
         for(int i=row-1;i>=0;i--)
         {
             if(board[i][col]==1)
@@ -11,7 +12,7 @@ public:
                 return false;
             }
         }
-        for(int i=row-1,j=col-1;i>=0 && j>=0 ;i--,j--)
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--)
         {
             if(board[i][j]==1)
             {
@@ -26,42 +27,45 @@ public:
             }
         }
         return true;
+        
     }
-    void solve(int row,int n,vector<vector<int>>&board)
+    void helper(vector<vector<int>>&board,int n,int row)
     {
         if(row==n)
         {
             temp.clear();
             for(int i=0;i<n;i++)
             {
-                string sol="";
+                string st="";
                 for(int j=0;j<n;j++)
                 {
                     if(board[i][j]==1)
                     {
-                        sol+="Q";
+                        st+='Q';
                     }
                     else{
-                        sol+=".";
+                        st+='.';
                     }
                 }
-                temp.push_back(sol);
+                temp.push_back(st);
             }
             ans.push_back(temp);
+            return ;
+            
         }
-        for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
         {
-            if(isPossible(row,i,n,board))
+            if(isPossible(row,j,board,n))
             {
-                board[row][i]=1;
-                solve(row+1,n,board);
-                board[row][i]=0;
+                board[row][j]=1;
+                helper(board,n,row+1);
+                board[row][j]=0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<int>>board(n,vector<int>(n,0));
-        solve(0,n,board);
+        helper(board,n,0);
         return ans;
     }
 };
