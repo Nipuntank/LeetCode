07dda21862f269cf181b9afc6dbe28b72b127d27
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2024-08-20 14:51:40 +0530

    Time: 124 ms (36.03%), Space: 84.3 MB (42.49%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -39,11 +39,11 @@ public:
             }
             st.push(i);
         }
-        int ans=0;
+        int maxi=0;
         for(int i=0;i<n;i++)
         {
-            ans=max(ans,heights[i]*(right[i]-left[i]+1));
+            maxi=max(maxi,heights[i]*(right[i]-left[i]+1));
         }
-        return ans;
+        return maxi;
     }
 };
