3dfe71da1af932a043f4258ddfe29e006f6934e9
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2025-02-03 16:12:24 +0530

    Time: 871 ms (58.18%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 SELECT employee_id,department_id
 FROM Employee
-WHERE employee_id IN( SELECT employee_id 
-    FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(*)=1) OR primary_flag='Y'
-GROUP BY  employee_id;
+WHERE primary_flag='Y'
+UNION 
+SELECT employee_id,department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(*)=1;
