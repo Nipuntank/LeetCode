976a164c4269d9ebc9d2326a86583366531e5799
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2025-02-23 17:45:29 +0530

    Time: 0 ms (100%), Space: 12.4 MB (91.59%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>&triangle,vector<vector<int>>&dp,int i,int j,int n)
-    {
-        if(i==n-1)
-        {
-            return triangle[i][j];
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int a=helper(triangle,dp,i+1,j,n);
-        int b=helper(triangle,dp,i+1,j+1,n);
-        return dp[i][j]=triangle[i][j]+min(a,b);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return helper(triangle,dp,0,0,n);
+        vector<int>curr(n,0);
+        vector<int>front(n,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            front[i]=triangle[n-1][i];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int a=triangle[i][j]+front[j];
+                int b=triangle[i][j]+front[j+1];
+                curr[j]=min(a,b);
+            }
+            front=curr;
+        }
+        return front[0];
     }
 };
