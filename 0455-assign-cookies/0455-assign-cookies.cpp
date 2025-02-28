dd9c38a80927b18664f942f41a913bd71ae3bbc6
Nipun tank <107953317+Nipuntank@users.noreply.github.com>
2025-02-28 16:58:00 +0530

    Time: 9 ms (53.17%), Space: 44.6 MB (95.82%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int m=g.size(), n=s.size();
+        int l=0,r=0;
+        while(l<n && r<m)
+        {
+            if(g[r]<=s[l])
+                r++;
+            l++;
+        }
+        return r;
+
+    }
+};
